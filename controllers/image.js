7ede9e170418cc089775cc0300de0aa475444afd
Ahmad Pathan <pathanahmad2334@gmail.com>
2023-06-10 19:50:04 +0530

    Merge bug

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,4 @@
+
 const handleImage = (req, res, knex) => {
     const { id } = req.body;
     console.log(id)
@@ -42,13 +43,10 @@ const handleAPICall = (req, res) => {
     };
     fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
         .then(response => {
-            console.log(response.status); // Check the response status code
-            console.log(response.headers.get('Content-Type'));
             console.log(response);
-            return response.text()
+            response.json()
         })
         .then(data => {
-            console.log(data)
             res.json(data)
         })
         .catch(error => {
